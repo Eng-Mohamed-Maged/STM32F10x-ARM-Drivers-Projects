c4aefcfaa00ed839620d97058fe0cea5b89d50c4
Eng-Mohamed-Maged <mohamed.maged.khalil1@gmail.com>
2023-09-11 02:04:18 +0300

    Initial Creation

---
+++ 01- LIB/01- STD_TYPES/STD_TYPES.h
@@ -0,0 +1,16 @@
+/*******************************************************/
+/* Author    :  Mohamed Maged                          */
+/* Date      :  1 September 2023                       */
+/* Version   :  V01									   */
+/* LOG       :  V01  -> Initial Creation               */
+/*                                                     */
+/*******************************************************/
+ #ifndef STD_TYPES_H
+ #define STD_TYPES_H
+ 
+ typedef  unsigned char       u8  ;
+ typedef  unsigned short int  u16 ;
+ typedef  unsigned long int   u32 ;
+
+
+ #endif
