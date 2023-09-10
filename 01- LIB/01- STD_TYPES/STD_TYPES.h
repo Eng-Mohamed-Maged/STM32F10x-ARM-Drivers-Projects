12c25a862f66a3b3f546bbdff52887ed4b734754
Eng-Mohamed-Maged <mohamed.maged.khalil1@gmail.com>
2023-09-11 02:21:54 +0300

    Update STD_TYPES.h
    V02 :+1:  Add unsigned types

--- 01- LIB/01- STD_TYPES/STD_TYPES.h
+++ 01- LIB/01- STD_TYPES/STD_TYPES.h
@@ -1,9 +1,9 @@
 /*******************************************************/
 /* Author    :  Mohamed Maged                          */
-/* Date      :  1 September 2023                       */
-/* Version   :  V01                                    */
+/* Date      :  11 September 2023                      */
+/* Version   :  V02									   */
 /* LOG       :  V01  -> Initial Creation               */
-/*                                                     */
+/*      		V02  -> Add unsigned types             */
 /*******************************************************/
  #ifndef STD_TYPES_H
  #define STD_TYPES_H
@@ -12,5 +12,9 @@
  typedef  unsigned short int  u16 ;
  typedef  unsigned long int   u32 ;
 
+
+ typedef  signed char       s8  ;
+ typedef  signed short int  s16 ;
+ typedef  signed long int   s32 ;
 
  #endif
