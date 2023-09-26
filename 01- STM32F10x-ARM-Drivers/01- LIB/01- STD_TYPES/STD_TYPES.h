2574faea1bb6f5beaf135f64e7f9ecf3e333f607
Eng-Mohamed-Maged <mohamed.maged.khalil1@gmail.com>
2023-09-27 02:10:10 +0300

    Update STD_TYPES.h
    Version 3.00

--- 01- STM32F10x-ARM-Drivers/01- LIB/01- STD_TYPES/STD_TYPES.h
+++ 01- STM32F10x-ARM-Drivers/01- LIB/01- STD_TYPES/STD_TYPES.h
@@ -1,9 +1,10 @@
 /*******************************************************/
 /* Author    :  Mohamed Maged                          */
-/* Date      :  11 September 2023                      */
-/* Version   :  V02									                           */
+/* Date      :  27 September 2023                      */
+/* Version   :  V03									   */
 /* LOG       :  V01  -> Initial Creation               */
-/*      		      V02  -> Add unsigned types             */
+/*      		V02  -> Add unsigned types             */
+/*              V03  -> Add u64 and s64                */
 /*******************************************************/
  #ifndef STD_TYPES_H
  #define STD_TYPES_H
@@ -11,10 +12,13 @@
  typedef  unsigned char       u8  ;
  typedef  unsigned short int  u16 ;
  typedef  unsigned long int   u32 ;
+ typedef  unsigned long long  u64 ;
 
 
- typedef  signed char       s8  ;
- typedef  signed short int  s16 ;
- typedef  signed long int   s32 ;
+
+ typedef  signed char         s8  ;
+ typedef  signed short int    s16 ;
+ typedef  signed long int     s32 ;
+ typedef  signed long long    s64 ;
 
  #endif
